eb973b31aa2dc890681706937372cd050c23dd01
Filipe Costa <a21702669@outlook.pt>
2020-05-27 19:42:20 +0100

    Posiçao do botao

--- Teste_Pelo_VisualStudio/Teste_Pelo_VisualStudio/Form1.Designer.cs
+++ Teste_Pelo_VisualStudio/Teste_Pelo_VisualStudio/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(124, 98);
+            this.button1.Location = new System.Drawing.Point(126, 138);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
